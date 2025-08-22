fb29ab78cbfe78b91aa9daf24eb3f1de396058d7
Muhammad Muazzain <90068510+MuhammadMuazzain@users.noreply.github.com>
2025-08-22 19:52:17 +0100

    Final

--- torch2tflite/converter.py
+++ torch2tflite/converter.py
@@ -46,6 +46,8 @@ class Torch2TFLiteConverter:
         # self.normalize = normalize
         # self.seed = seed
         # self.normalize = normalize
+        # self.seed = seed
+        # self.normalize = normalize
         self.seed = seed
         self.normalize = normalize
 
