c53ab6da5cac8de318f6f6a1c02ee56e71bf4970
Muhammad Muazzain <90068510+MuhammadMuazzain@users.noreply.github.com>
2025-09-01 03:07:56 +0500

    Commit

--- torch2tflite/converter.py
+++ torch2tflite/converter.py
@@ -127,6 +127,7 @@ class Torch2TFLiteConverter:
         # return interpret
         # return interpret
         # return interpret
+        # return interpret
         return interpret
 
     @staticmethod
